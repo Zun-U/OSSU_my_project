c1009d8be7eb9dde04d2fb204958d8f7a757699f
Zun-U <hello.tra.uenishi@gmail.com>
2023-05-02 00:30:59 +0900

    ossu try / catch

--- IfStatement.py
+++ IfStatement.py
@@ -104,7 +104,147 @@ print('All done')
 q = 4
 
 if q > 2 :
-  print('Bigger')
+  print('Bigger q')
 else :
-  print('Smaller')
-print('All done')
+  print('Smaller q')
+print('All done q')
+
+
+
+# Multi-way
+# それぞれが順番に評価されて、falseなら行がスキップされる。（つまり、評価は『全部』行われる）
+
+n = 5
+
+if n < 2 : # 評価する
+  print('small n')
+elif n < 10 : # 評価する
+  print('Medium n')
+else : # 評価する
+  print('LARGE')
+print('All done n')
+
+
+# 実行できないパターンもある　No Else
+
+p = 50
+if p < 2 :
+  print('small p')
+elif p < 10 :
+  print('Medium p')
+print('All done p')
+
+# 「else」がないと評価式のいずれか一つは実行されるという『保証が無い』
+# ❐ すべてのものがfalseの場合、elseが実行される ❐
+
+
+
+# 多くの「elif」を持つことができる
+
+g = 5
+if g < 2 :
+  print('small g')
+elif g < 10 :
+  print('Medium g')
+elif g < 40 :
+  print('big')
+else :
+  print('Ginormous')
+
+
+# Multi-way Pazzles
+# case1
+a = 4
+
+if a < 2 :
+  print('Below 2')
+elif a >= 2 :
+  print('Two or more')
+# elseを通ることはない
+else :
+  print('some thing else')
+
+# case2
+
+if a < 2 :
+  print('Below 2')
+elif a < 20 :
+  print('Below 20')
+
+# この「elif」は通らない
+# なぜなら、上の評価式でtrue判定が出たから
+# ❐❐　trueがでるとそこで分岐が終了し、以下の評価式は何も実行されない　❐❐
+elif a < 10 :
+  print('Below 10')
+
+else :
+  print('some thing else')
+
+
+
+# The try / except Structure
+# 壊れて爆発する可能性のあるコードを上手く処理する方法
+
+astr = 'Hello Bob'
+
+try:
+  # 数値以外の文字列が代入されたため、『トレースバック』が実行される
+  # ※例外の原因となる自分のコードに辿り着きます 。 この呼び出し履歴を遡って追跡することを「Traceback」といいます。
+  istr = int(astr)
+
+# 「try」が動作すると、「except」が無視される
+# 「try」が失敗すると、「expect」、例外が実行される
+except:
+  # 例外が発生したことを知らせる『フラグ』の様なものとして「-1」を代入している（ここでは、「-1」が代入されたら例外処理flagが立ちましたよの意味としている）
+  istr = -1
+print('First', istr)
+
+astr = '123'
+try:
+  istr = int(astr)
+# 「expect」はコードで問題が発生した場合のみトリガーされる
+except:
+  istr = -1
+print('Second', istr)
+
+
+
+# try / except
+
+astr = 'Bob'
+try:
+  print('Hello', 'ここまでは実行される')
+  # コードが爆発(エラー発生)するまでは処理しようと試みる
+  # ❐ 爆発が発生した箇所以降のtryブロックは実行されない　❐
+  istr = int(astr)
+  print('There')
+except:
+  istr = -1
+print('Done', istr, 'tryの途中から爆発が発生し、exceptに移行')
+
+
+# try / exceptを正しい形にしたのが↓
+print('Hello')
+try:
+  istr = int(astr)
+except:
+  istr = -1
+print('There')
+print('Done', istr)
+
+
+
+
+
+# Simple try /except
+# より実世界のコード
+
+rawstr = input('Enter a number')
+try:
+  ival = int(rawstr)
+except:
+  ival = -1
+if ival > 0 :
+  print('Nice work')
+else:
+  print('Not a number')
