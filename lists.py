0c5b4b82b21f2cc0b34f68793c8600eaccaf69a5
Zun-U <hello.tra.uenishi@gmail.com>
2023-05-14 21:45:37 +0900

    lists loops

--- lists.py
+++ lists.py
@@ -82,11 +82,88 @@ fruit = 'Banana'
 # fruit[0] = 'b' >>> トレースバックが発生する
 #「文字列」は一度作ると変更不可能ということ
 
-x = fruit.lower()
+x = fruit.lower() # 変数「fruit」を小文字にした「コピー」を、変数「x」に代入している
 print(x)
+# ❐❐　ポイントは、元の「fruit」は変わらないってこと　❐❐
+print(fruit)
 
 lotto = [2, 14, 26, 41, 63]
 print(lotto)
+
+# 変数に単に入力するのではなく、変数内のこの位置に入力するように左側に括弧構文を置くことで、リストに項目を割り当てることができる。
 lotto[2] = 28
 print(lotto)
-# 整数は変更 "可能"
+
+
+
+
+
+# How Long is a List
+greet = 'Hello Bob'
+print(len(greet))
+# |H|e|l|l|o| |B|o|b|
+# |0|1|2|3|4|5|6|7|8|
+# >>> 9文字の文字列
+
+x = [1, 2, 'joe', 99]
+print(len(x))
+# \1\2\'joe'\99\
+# \0\1\  2  \ 3\
+# >>> List中の要素数
+
+
+
+
+# Using the range function
+print(range(4)) # rangeは、返したい数値の数をパラメータとして受け取る
+# [0, 1, 2, 3]　>>>  [0 : 4]
+
+friends = ['Joseph', 'Glenn', 'Sally']
+print(len(friends))
+# |'Joseph'|'Glenn'|'Sally'|
+# |   0    |   1   |   2   |
+# >>> 要素数は「3」
+
+print(range(len(friends)))
+# [0, 1, 2] >>> [0 : 3]
+
+
+
+# A tale of two loops
+friends = ['Joseph', 'Glenn', 'Sally']
+
+# List自体をloopする
+for friend in friends :
+  print('Happy New Year:', friend)
+
+# もう少し洗練されたloop
+# リストの位置が分かる
+for i in range(len(friends)) : # 位置を通過する独自の変数「i」 >>> 通過するのは[0 : 3]、つまり[0, 1, 2]
+  friend = friends[i]
+  print('Happy New Year:', friend)
+
+
+
+# Concatenating lists using +
+a = [1, 2, 3]
+b = [4, 5, 6]
+
+# 「+」は、文字列、整数、浮動小数点、そして 'リスト' も追加できる
+# 新しい変数を生成して、それを「c」に代入している
+c = a + b
+
+print(c) # リストの連結
+print(a) # 他と同様、元の変数は変更されない
+
+
+
+# Lists can be sliced using :
+# listsのスライス
+t = [9, 41, 12, 3, 74, 15]
+print(t[1:3])
+
+print(t[:4])
+
+print(t[3:])
+
+print(t[:])
